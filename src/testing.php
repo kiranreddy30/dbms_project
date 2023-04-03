ce6bb64f56dba92373f998c02b7f09eac45945dd
kiranreddy30 <pkiransai487@gmail.com>
2023-04-03 11:46:27 +0530

    first_commit

---
+++ src/testing.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/f9fb1558da.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- <a href="index.php"><i class="fa-bold fa-right-to-bracket"></i> -->
+   
+            <i class="fa-solid fa-house"></i>
+
+</a>
+</body>
+</html>
