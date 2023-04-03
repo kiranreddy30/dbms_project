7b7141bd6ddf5f4a4683115b32d594a5b79c7a89
kiranreddy30 <pkiransai487@gmail.com>
2023-04-03 16:40:42 +0530

    modif

--- src/login.php
+++ src/login.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
   <!-- Design by foolishdeveloper.com -->
-    <title>LOGIN</title>
-   
+    <title>Glassmorphism login Form Tutorial in html css</title>
  
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
@@ -22,7 +21,7 @@ body{
 }
 .background{
     width: 430px;
-    height: 520px;.
+    height: 520px;
     position: absolute;
     transform: translate(-50%,-50%);
     left: 50%;
